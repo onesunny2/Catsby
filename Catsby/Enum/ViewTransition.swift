0a942b10865c4b10fe52c8b708898251c311d4d9
onesunny2 <ippne996@gmail.com>
2025-01-25 17:46:42 +0900

    [feat] 화면전환에 사용할 enum, extension 구현

---
+++ Catsby/Enum/ViewTransition.swift
@@ -0,0 +1,14 @@
+//
+//  ViewTransition.swift
+//  Catsby
+//
+//  Created by Lee Wonsun on 1/25/25.
+//
+
+import UIKit
+
+enum ViewTransition {
+    case push
+    case modal
+    case windowRoot
+}
