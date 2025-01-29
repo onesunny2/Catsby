6c9bc00bd66f772912d51eca91cc97f67b5d8716
onesunny2 <ippne996@gmail.com>
2025-01-30 02:56:20 +0900

    [design] SettingTab 레이아웃 설정

---
+++ Catsby/TabBarController/Setting/Cell/SettingTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  SettingTableViewCell.swift
+//  Catsby
+//
+//  Created by Lee Wonsun on 1/30/25.
+//
+
+import UIKit
+import SnapKit
+
+class SettingTableViewCell: UITableViewCell, BaseConfigure {
+    
+    static let id = "SettingTableViewCell"
+    
+    let titleLabel: BaseLabel
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        titleLabel = BaseLabel(text: "", align: .left, size: 15, weight: .regular)
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .catsBlack
+        configHierarchy()
+        configLayout()
+    }
+    
+    func configHierarchy() {
+        self.addSubview(titleLabel)
+    }
+    
+    func configLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.leading.centerY.equalToSuperview()
+        }
+    }
+    
+    func getTitle(_ title: String) {
+        titleLabel.text = title
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
