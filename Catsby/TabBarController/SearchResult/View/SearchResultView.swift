ee3bf76e9194a4c24e6a532affb057dbb5884971
onesunny2 <ippne996@gmail.com>
2025-01-30 16:36:46 +0900

    [feat] 메인화면에서 검색화면으로 화면전환 연결

---
+++ Catsby/TabBarController/SearchResult/View/SearchResultView.swift
@@ -0,0 +1,28 @@
+//
+//  SearchResultView.swift
+//  Catsby
+//
+//  Created by Lee Wonsun on 1/30/25.
+//
+
+import Foundation
+
+final class SearchResultView: BaseView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .catsBlack
+        
+//        configHierarchy()
+//        configLayout()
+    }
+    
+//    override func configHierarchy() {
+//        <#code#>
+//    }
+//    
+//    override func configLayout() {
+//        <#code#>
+//    }
+}
---
+++ Catsby/TabBarController/SearchResult/ViewController/SearchResultViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultViewController.swift
+//  Catsby
+//
+//  Created by Lee Wonsun on 1/30/25.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+    
+    private let mainView = SearchResultView()
+    
+    override func loadView() {
+        view = mainView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
