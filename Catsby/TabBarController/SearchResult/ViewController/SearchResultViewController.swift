e63b2248314738e09ce4e69f6c6e2e52398dd2f9
onesunny2 <ippne996@gmail.com>
2025-01-30 17:01:36 +0900

    [design] 네비게이션 타이틀 및 서치바 속성 설정

--- Catsby/TabBarController/SearchResult/ViewController/SearchResultViewController.swift
+++ Catsby/TabBarController/SearchResult/ViewController/SearchResultViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class SearchResultViewController: UIViewController {
+class SearchResultViewController: UIViewController, UISearchBarDelegate, UISearchControllerDelegate {
     
     private let mainView = SearchResultView()
+    private let searchController = UISearchController(searchResultsController: nil)
     
     override func loadView() {
         view = mainView
@@ -18,7 +19,26 @@ class SearchResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        setNavigation()
     }
 
+    
+    // 네비게이션 타이틀 및 서치바 설정
+    private func setNavigation() {
+        navigationItem.title = "영화 검색"
+        
+        searchController.delegate = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "검색하고 싶은 영화를 적어주세요 :)"
+        searchController.searchBar.searchBarStyle = .minimal
+
+        navigationItem.searchController = searchController
+        
+        guard let textfield = searchController.searchBar.value(forKey: "searchField") as? UITextField else { return }
+        textfield.textColor = .catsWhite
+        textfield.attributedPlaceholder = NSAttributedString(string: textfield.placeholder ?? "", attributes: [NSAttributedString.Key.foregroundColor: UIColor.catsDarkgray])
+        
+        guard let image = textfield.leftView else { return }
+        image.tintColor = .catsWhite
+    }
 }
