bf1c52dd3a9fb8b4f3d9209747541d896fe431c1
onesunny2 <ippne996@gmail.com>
2025-02-10 01:37:52 +0900

    [chore] 버튼 색상 전체 filled 하게 변경

--- Catsby/FirstProfileSetting/View/ProfileNicknameView.swift
+++ Catsby/FirstProfileSetting/View/ProfileNicknameView.swift
@@ -55,7 +55,7 @@ final class ProfileNicknameView: BaseView {
         
         checkNickname = BaseLabel(text: " ", align: .left, color: .catsMain, size: 16, weight: .regular)
         
-        completeButton = BaseButton(title: "완료", size: 16, weight: .bold, bgColor: .catsBlack, foreColor: .catsMain)
+        completeButton = BaseButton(title: "완료", size: 16, weight: .bold, bgColor: .catsMain, foreColor: .catsWhite)
         completeButton.capsuleStyle()
         completeButton.stroke(.catsMain, 2)
         
