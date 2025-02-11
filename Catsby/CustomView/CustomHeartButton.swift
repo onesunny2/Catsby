40a56057a4e76101d0f74815dcd72ca621d7268e
onesunny2 <ippne996@gmail.com>
2025-02-11 18:53:53 +0900

    [refactor] customHeartButton 내장 로직 변경

--- Catsby/CustomView/CustomHeartButton.swift
+++ Catsby/CustomView/CustomHeartButton.swift
@@ -9,24 +9,49 @@ import UIKit
 
 final class CustomHeartButton: UIButton {
     
-    init(movieID: Int) {
+    override var isSelected: Bool {
+        didSet {
+            self.configuration = isSelected ? configButton(.like) : configButton(.unlike)
+        }
+    }
+    
+    init() {
         super.init(frame: .zero)
-        
-        let stringID = String(movieID)
-        let isLiked: Bool = UserDefaultsManager.shared.getDicData(type: .likeButton)[stringID] ?? false
+        isSelected = false
+    }
+    
+    private func configButton(_ status: Status) -> UIButton.Configuration {
         
         var config = UIButton.Configuration.filled()
         
-        config.image = UIImage(systemName: isLiked ? "heart.fill" : "heart")
+        config.image = UIImage(systemName: status.imgName)
         config.baseBackgroundColor = .clear
         config.baseForegroundColor = .catsMain
         
-        self.configuration = config
+        return config
     }
     
-    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        super.touchesEnded(touches, with: event)
+
+        isSelected.toggle()
+    }
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CustomHeartButton {
+    
+    enum Status: String {
+        case like = "heart.fill"
+        case unlike = "heart"
+        
+        var imgName: String {
+            return self.rawValue
+        }
     }
 }
