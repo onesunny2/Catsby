513c3dc17888d471c5c07d4568f3f07b8f9a3ee1
onesunny2 <ippne996@gmail.com>
2025-02-13 09:26:34 +0900

    [refactor] VC의 네트워크 통신 VM로 이동

--- Catsby/ViewModel/DetailViewModel.swift
+++ Catsby/ViewModel/DetailViewModel.swift
@@ -36,6 +36,61 @@ final class DetailViewModel: BaseViewModel {
     }
     
     func transformBinds() {
+        input.callRequest.bind { [weak self] _ in
+            self?.callRequest()
+        }
+
+    }
+    
+    func genreList() -> [String] {
         
+        let list = backdropDetails.genre.map { Genre.genreList[$0] ?? "" }.filter { $0.count <= 2 }
+        
+        return list
+    }
+}
+
+extension DetailViewModel {
+    
+    private func callRequest() {
+        
+        guard let movie else {
+            print("movie data nill")
+            return
+        }
+        
+        // backdrop, poster 정보
+        group.enter()
+        NetworkManager.shared.callRequest(type: ImageMovie.self, api: .image(movieID: movie.id)) { [weak self] response in
+            
+            guard let self else { return }
+            detailData.imgBackdrops = response.backdrops
+            detailData.imgPosters = response.posters
+            group.leave()
+            
+        } failHandler: { [weak self] in
+            guard let self else { return }
+            print("backdrop, poster error")
+            group.leave()
+        }
+
+        // cast 정보
+        group.enter()
+        NetworkManager.shared.callRequest(type: CreditMovie.self, api: .credit(movieID: movie.id)) { [weak self] response in
+            
+            guard let self else { return }
+            detailData.casts = response.cast
+            group.leave()
+            
+        } failHandler: { [weak self] in
+            guard let self else { return }
+            print("cast error")
+            group.leave()
+        }
+
+        group.notify(queue: .main) { [weak self] in
+            guard let self else { return }
+            output.endCallRequest.value = ()
+        }
     }
 }
